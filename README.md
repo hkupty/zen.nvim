a7ccb3f495430977094b894dfe6f0756df04a403
Henry Kupty <hkupty@gmail.com>
2016-11-08 10:49:24 -0200

    Initial Version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # zen.nvim
 A simple nvim ui framework for python
+
+## Using this framework
+
+While this is a simple python module that expects you to have neovim installed,
+it does not require anything explicitly and can simply be embedded into you
+application.
+
+One can do so in several ways:
+
+  * Adding this as module a git submodule of your application [preferred]
+  * Requiring the user to explicitly install
+  * Copying the files from this module to your app
+
+### Using as submodule
+
+Simply `git submodule add 
---
+++ src/ui.py
@@ -0,0 +1,83 @@
+""" Zen ui.
+
+This is a pretty straightforward ui framework for neovim.
+
+This allows standardizations and quick handling of common ui elements
+The idea of this framework is to provide common solutions without hassle.
+"""
+
+
+class EmptyPromptError(Exception):
+    """ User aborted prompt. """
+    pass
+
+
+def prompt(nvim):
+    nvim.command("inputsave")
+    ret = nvim.command_output("input", "iron> {}: ".format(msg))
+    nvim.command("inputrestore")
+
+    if not ret:
+        raise EmptyPromptError(msg)
+
+    return ret
+
+
+def selection_window(nvim, **options):
+    size = len(options["select_options"]) + 4
+
+    if "header" in options:
+        size += 1
+
+    size = min(size, 15)
+
+    nvim.command("botright {} spl | enew".format(size))
+    buf_id = nvim.call("bufnr", "$")
+    sw_buf = nvim.buffers[buf_id]
+
+    if "header" in options:
+        sw_buf[0] = options["header"]
+
+    select_options = options["select_options"]
+
+    [nvim.command(i) for i in [
+        "map <buffer> {} {}".format(k, c) for k, _, c in select_options
+    ]]
+
+
+    text = ["Select an option below:", ""] + [
+        "    ({}) {}".format(k, t) for k, t, _ in select_options
+    ]
+
+    sw_buf.append(text)
+
+
+def build_window(nvim, **kwargs):
+    orientation = kwargs.get('orientation', "topleft vertical split")
+    cmds = [orientation, "enew"]
+
+    if 'close' in kwargs:
+        cmds.append('nmap <buffer> q :bd! %<CR>')
+
+    if 'commands' in kwargs:
+        cmds.extend(kwargs['commands'])
+
+    try:
+        nvim.command('|'.join(cmds))
+    except:
+        return None
+    else:
+        return nvim.funcs.bufnr('$')
+
+
+def toggleable_buffer(nvim, buf_id, create_new_fn, **kwargs):
+    orientation = kwargs.get('orientation', 'vertical split')
+    win_nr = nvin.funcs.bufwinnr(buf_id)
+
+    if win_nr == -1:
+        if bufname(buf_id) == "":
+            create_new_fn()
+        else:
+            nvim.command('{} | b {}'.format(orientation, buf_id))
+    else:
+        nvim.command('{} wincmd w | q | stopinsert'.format(win_nr))
